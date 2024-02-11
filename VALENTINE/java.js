9e9869bf944006796f63b6f737adc8a6e72999ba
A-Swigg <132660292+A-Swigg@users.noreply.github.com>
2024-02-11 19:21:23 +0100

    Add files via upload

--- VALENTINE/java.js
+++ VALENTINE/java.js
@@ -1,4 +1,4 @@
-    const compliments = ["Søte", "Fine", "Vakre", "Flinke", "Flotte", "Sexy", "Heite", "Beste", "Lekre", "Attraktive", "Pene", "Snille", "Sjarmerende", "Glamorøse", "Nydelige", "Nusselige", "Perfekte"];
+    const compliments = ["Søte", "Fine", "Vakre", "Flinke", "Flotte", "Sexy", "Heite", "Beste", "Lekre", "Attraktive", "Pene", "Snille", "Sjarmerende", "Glamorøse", "Nydelige", "Nusselige", "Perfekte", "Thicc", "Skjønne", "Submissive", "Ømfintlige", "Digge"];
 
     document.getElementById('complimentButton').addEventListener('click', () => {
         const randomIndex = Math.floor(Math.random() * compliments.length);
